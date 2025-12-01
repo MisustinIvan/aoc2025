3aa5ed473fa868ba8b903f5e162625f7d5f33958
MisustinIvan <ivanmis596@gmail.com>
2025-12-01 20:51:34 +0100

    day 1 part 1 and 2 solutions

---
+++ lib/day01.ex
@@ -0,0 +1,55 @@
+defmodule Aoc2025.Day01 do
+  def parse_input(input) do
+    input
+    |> String.split("\n")
+    |> Enum.map(fn instruction ->
+      case instruction do
+        "R" <> amount ->
+          {num, _} = Integer.parse(amount)
+          num
+
+        "L" <> amount ->
+          {num, _} = Integer.parse(amount)
+          -num
+      end
+    end)
+  end
+
+  def solve(input, :part1) do
+    input
+    |> parse_input
+    |> Enum.reduce({0, 50}, fn move, {acc, pos} ->
+      case Integer.mod(pos + move, 100) do
+        0 -> {acc + 1, 0}
+        n -> {acc, n}
+      end
+    end)
+  end
+
+  def solve(input, :part2) do
+    input
+    |> parse_input
+    |> Enum.reduce({0, 50}, fn move, {acc, pos} ->
+      new_pos = pos + move
+      new_pos_wrapped = Integer.mod(new_pos, 100)
+
+      cond do
+        new_pos >= 100 ->
+          {acc + div(new_pos, 100), new_pos_wrapped}
+
+        new_pos <= 0 ->
+          {
+            acc + div(new_pos, -100) + 1 +
+              cond do
+                pos == 0 -> -1
+                true -> 0
+              end,
+            new_pos_wrapped
+          }
+
+        true ->
+          {acc, new_pos_wrapped}
+      end
+    end)
+  end
+end
