e6c6c0fc922429002592f1283ad47ce9e26225e5
MisustinIvan <ivanmis596@gmail.com>
2025-12-02 18:08:01 +0100

    add test boilerplate

---
+++ test/day00_test.exs
@@ -0,0 +1,16 @@
+defmodule Day00Test do
+  alias Aoc2025.Day00
+  use ExUnit.Case, async: true
+
+  defp test_data do
+    ""
+  end
+
+  test "day00_part1" do
+    assert Day00.solve(test_data(), :part1) == :not_implemented
+  end
+
+  test "day00_part2" do
+    assert Day00.solve(test_data(), :part2) == :not_implemented
+  end
+end
