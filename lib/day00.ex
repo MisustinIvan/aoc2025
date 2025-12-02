2a1ead6c02262259e8e7c6e3de1b5e528dc5b117
MisustinIvan <ivanmis596@gmail.com>
2025-12-02 18:05:59 +0100

    add boilerplate template

---
+++ lib/day00.ex
@@ -0,0 +1,16 @@
+defmodule Aoc2025.Day00 do
+  @spec parse_input(String.t()) :: atom
+  defp parse_input(_input) do
+    :not_implemented
+  end
+
+  @spec solve(String.t(), :part1) :: atom
+  def solve(_input, :part1) do
+    :not_implemented
+  end
+
+  @spec solve(String.t(), :part2) :: atom
+  def solve(_input, :part2) do
+    :not_implemented
+  end
+end
